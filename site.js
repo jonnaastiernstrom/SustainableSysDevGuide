f9e972dad71dcf91c9e42f16fdd3c93bce01336c
jonnaastiernstrom <jonnaastiernstrom@hotmail.com>
2024-03-08 10:20:01 +0100

    removed carousel timer

--- site.js
+++ site.js
@@ -24,7 +24,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-let slideInterval = setInterval(function() { plusSlides(1); }, 5000);
+
 
 
 
